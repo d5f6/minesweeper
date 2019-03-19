fe8b9be85f2c7ebef569e1be5ad677d10780cda7
d5f6 <dennisfreitag@gmail.com>
2019-03-19 21:05:17 +0100

    grid added. Some further work to do...

--- main.lua
+++ main.lua
@@ -9,7 +9,19 @@ require 'src/dependencies'
 function love.load()
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT)
   
-  love.graphics.setFont(gFonts['pixeled'])
+  math.randomseed(os.time())
+
+  grid = {}
+
+  for y = 1, 10 do
+    table.insert(grid, {})
+
+    for x = 1, 10 do
+      table.insert(grid[y], math.random(2))
+    end
+  end
+
+  love.graphics.setFont(gFonts['start'])
 end
 
 function love.keypressed(key)
@@ -23,6 +35,19 @@ end
 
 function love.draw()
   push:start()
-  love.graphics.print('000')
+
+  love.graphics.clear(0.2, 0.2, 0.2, 1)
+  love.graphics.print('120')
+  love.graphics.printf('0', 0, 0, VIRTUAL_WIDTH, 'right')
+  
+  for y = 1, 10 do
+    for x = 1, 10 do
+      love.graphics.draw(grid[x][y] == 1 
+        and gTextures['tile'] 
+        or gTextures['tile-depressed'],
+        (x - 1) * TILE_SIZE, (y - 1) * TILE_SIZE)
+    end
+  end
+
   push:finish()
 end
--- src/dependencies.lua
+++ src/dependencies.lua
@@ -8,8 +8,13 @@ push = require 'lib/push'
 
 require 'src/constants'
 
+gTextures = {
+  ['tile'] = love.graphics.newImage('graphics/tile.png'),
+  ['tile-depressed'] = love.graphics.newImage('graphics/tile_depressed.png'),
+}
+
 gFonts = {
   ['poco'] = love.graphics.newFont('fonts/Poco.ttf', 20),
   ['pixeled'] = love.graphics.newFont('fonts/Pixeled.ttf', 10),
-  ['start'] = love.graphics.newFont('fonts/start.ttf', 20),
+  ['start'] = love.graphics.newFont('fonts/start.ttf', 16),
 }
