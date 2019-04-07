f002aaf1976c9f4ad617ae46f5d4d0555bbb8b41
dennisfreitag <dennisfreitag@gmail.com>
2019-04-07 14:18:18 +0200

    implemtent a simple improvement

--- src/GameGrid.lua
+++ src/GameGrid.lua
@@ -33,63 +33,66 @@ function GameGrid:calculateNumbers()
     for x = 1, self.width do
       local numBombNeighbors = 0
 
-      -- check top left
-      if x > 1 and y > 1 then
-        if self.grid[y - 1][x - 1].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
-        end
-      end
+      if not self.grid[y][x].isBomb then
 
-      -- check top
-      if y > 1 then
-        if self.grid[y - 1][x].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
+        -- check top left
+        if x > 1 and y > 1 then
+          if self.grid[y - 1][x - 1].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
         end
-      end
 
-      -- check top right
-      if y > 1 and x < self.width then
-        if self.grid[y - 1][x + 1].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
+        -- check top
+        if y > 1 then
+          if self.grid[y - 1][x].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
         end
-      end
 
-      -- check left
-      if x > 1 then
-         if self.grid[y][x - 1].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
+        -- check top right
+        if y > 1 and x < self.width then
+          if self.grid[y - 1][x + 1].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
         end
-      end
 
-      -- check right
-      if x < self.width then
-         if self.grid[y][x + 1].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
+        -- check left
+        if x > 1 then
+          if self.grid[y][x - 1].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
         end
-      end
 
-      -- check bottom left
-      if x > 1 and y < self.height then
-        if self.grid[y + 1][x - 1].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
+        -- check right
+        if x < self.width then
+          if self.grid[y][x + 1].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
         end
-      end
 
-      -- check bottom
-      if y < self.height then
-        if self.grid[y + 1][x].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
+        -- check bottom left
+        if x > 1 and y < self.height then
+          if self.grid[y + 1][x - 1].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
         end
-      end
 
-      -- check bottom right
-      if y < self.height and x < self.width then
-        if self.grid[y + 1][x + 1].isBomb then
-          numBombNeighbors = numBombNeighbors + 1
+        -- check bottom
+        if y < self.height then
+          if self.grid[y + 1][x].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
         end
-      end
 
-      self.grid[y][x].numBombNeighbors = numBombNeighbors
+        -- check bottom right
+        if y < self.height and x < self.width then
+          if self.grid[y + 1][x + 1].isBomb then
+            numBombNeighbors = numBombNeighbors + 1
+          end
+        end
+
+        self.grid[y][x].numBombNeighbors = numBombNeighbors
+      end
     end
   end
 end
